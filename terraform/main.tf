6b4ad95b049b8555978a978b7ad31478c4d96722
Corey Wanless <coreywanless@gmail.com>
2020-10-20 17:22:34 -0500

    changed to centos7

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ data "vsphere_network" "network" {
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "build-centos8-${var.env}"
+  name          = "build-centos7-${var.env}"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
