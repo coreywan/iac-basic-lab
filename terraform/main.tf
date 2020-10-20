9898d37c1d8444a53969035af5918f74205973a4
Corey Wanless <coreywanless@gmail.com>
2020-10-20 16:03:55 -0500

    lint stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ variable "env" {
 
 provider "vsphere" {
     user                    = "administrator@vsphere.local"
-    password                = "${var.vsphere_password}"
+    password                = var.vsphere_password
     vsphere_server          = "vc.lab.local"
     allow_unverified_ssl    = true
 }
@@ -22,7 +22,7 @@ data "vsphere_datastore" "datastore" {
 
 data "vsphere_compute_cluster" "cluster" {
   name          = "Compute"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 data "vsphere_network" "network" {
   name          = "Management Network"
@@ -37,29 +37,29 @@ data "vsphere_virtual_machine" "template" {
 
 resource "vsphere_virtual_machine" "gitlab" {
   name             = "${var.env}-gitlab"
-  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
 
   num_cpus = 2
   memory   = 2048
-  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+  guest_id = data.vsphere_virtual_machine.template.guest_id
 
-  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
 
   network_interface {
-    network_id   = "${data.vsphere_network.network.id}"
-    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
   }
 
   disk {
     label            = "disk0"
-    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
-    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
-    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
   }
 
   clone {
-    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    template_uuid = data.vsphere_virtual_machine.template.id
 
     customize {
       linux_options {
